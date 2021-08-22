8ad76b568a3699289cc504568d012ae4dde49e4e
fadiHB <eng.fadi.hb@gmail.com>
2021-08-22 22:46:21 +0300

    part1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React  from 'react';
 import Header from './Header';
 import IsLoadingAndError from './IsLoadingAndError';
 import Footer from './Footer';
@@ -7,6 +7,10 @@ import {
   Switch,
   Route
 } from "react-router-dom";
+import {withAuth0} from '@auth0/auth0-react'
+import MyFavoriteBooks from './BestBooks';
+import Login from './Login';
+import Profile from './Profile';
 
 class App extends React.Component {
 
@@ -20,8 +24,10 @@ class App extends React.Component {
             <Switch>
               <Route exact path="/">
                 {/* TODO: if the user is logged in, render the `BestBooks` component, if they are not, render the `Login` component */}
+                {this.props.auth0.isAuthenticated? <MyFavoriteBooks />: <Login />}
               </Route>
               {/* TODO: add a route with a path of '/profile' that renders a `Profile` component */}
+              <Route path='/profile' component={Profile}/>
             </Switch>
             <Footer />
           </IsLoadingAndError>
@@ -31,4 +37,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default withAuth0(App);
--- src/Header.js
+++ src/Header.js
@@ -3,6 +3,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 import './Header.css';
+import {withAuth0} from '@auth0/auth0-react'
+import LogoutButton from './components/LogOutButton';
+import LoginButton from './components/LogInButton';
 
 class Header extends React.Component {
   render() {
@@ -12,9 +15,10 @@ class Header extends React.Component {
         <Link to="/">Home</Link>
         <Link to="/profile">Profile</Link>
         {/* TODO: if the user is logged in, render the `LogoutButton` - if the user is logged out, render the `LoginButton` */}
+        {this.props.auth0.isAuthenticated ? <LogoutButton /> : <LoginButton />}
       </Navbar>
     );
   }
 }
 
-export default Header;
+export default withAuth0(Header);
