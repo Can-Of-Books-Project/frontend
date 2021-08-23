aafb5cdb52f0ba417b6715ebe8d3943f22d46a56
fadiHB <eng.fadi.hb@gmail.com>
2021-08-23 14:42:45 +0300

    env

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ import { Auth0Provider } from "@auth0/auth0-react";
 // TODO: wrap everything in Auth0
 ReactDOM.render(
   <Auth0Provider
-    // domain={process.env.REACT_APP_DOMAIN}
-    // clientId={process.env.REACT_APP_CLIENT_ID}
-    domain='dev-nnqsw7ij.us.auth0.com'
-    clientId='pahxziJBhgnfnrhOqkLvKBFA8UB0vREF'
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
+    // domain='dev-nnqsw7ij.us.auth0.com'
+    // clientId='pahxziJBhgnfnrhOqkLvKBFA8UB0vREF'
     redirectUri={window.location.origin}
   >
     <App />
