dd7c910445089391e231a7661c312f3834342daf
fadiHB <eng.fadi.hb@gmail.com>
2021-09-04 18:48:20 +0300

    unignore the .env

--- src/API/books_api.js
+++ src/API/books_api.js
@@ -4,12 +4,12 @@ const REACT_APP_API_URL = process.env.REACT_APP_API_URL
 
 export async function getUserBooks(email) {
     const url = REACT_APP_API_URL + '/api/booksByEmail';
-    return await axios.post(url, {authorEmail:email}).then(response => response.data )
+    return await axios.post(url, { authorEmail: email }).then(response => response.data)
 }
 
 export async function CreateUser(name, email) {
     const url = REACT_APP_API_URL + '/api/addUser';
-    await axios.post(url, {name:name, email:email}).then(data => {
+    await axios.post(url, { name: name, email: email }).then(data => {
     }).catch(error => {
         alert("Ops .. Something Wrong Occurce while adding new user")
         // console.log(error)
@@ -18,11 +18,10 @@ export async function CreateUser(name, email) {
 
 export async function addBook(title, description, status, img, email) {
     const url = REACT_APP_API_URL + '/api/addBook';
-    await axios.post(url, {title:title, description:description, status:status, img:img, email:email}).catch(error => {
+    await axios.post(url, { title: title, description: description, status: status, img: img, email: email }).catch(error => {
         alert("Ops .. Something Wrong Occurce while adding new books")
         // console.log(error)
     })
-
 }
 
 
@@ -32,13 +31,12 @@ export async function deleteBook(id) {
         alert("Ops .. Something Wrong Occurce while deleting")
         // console.log(error)
     })
-
 }
 
 
 export async function updateBook(title, description, status, img, pk) {
     const url = REACT_APP_API_URL + '/api/update/' + pk.toString();
-    await axios.post(url, {title:title, description:description, status:status, img:img}).catch(error => {
+    await axios.post(url, { title: title, description: description, status: status, img: img }).catch(error => {
         alert("Ops .. Something Wrong Occurce while updating")
         // console.log(error)
     })
